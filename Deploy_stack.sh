eee1fa5190876fe66b060dcb5e398c7f069c3cd9
wiinches <winchejg@gmail.com>
2018-05-18 14:55:50 -0400

    Deploy Elastic_stack

---
+++ Deploy_stack.sh
@@ -0,0 +1,56 @@
+yum install -y docker
+echo "DOCKER_STORAGE_OPTIONS="--storage-driver devicemapper "" > /etc/sysconfig/docker-storage
+systemctl enable docker
+systemctl start docker
+#Run interface.sh to duplicate your active port <em1, eno0 etc> do this for each instance of elasticsearch
+read -p "Domain Name (e.g. test.lan): " DOMAIN_NAME
+read -p "How many Elastic Nodes?: " NODES
+read -p "which interface would you like to clone?: " INTERFACE
+read -p "Configuration and data storage directory? (e.g /data): " DATA_DIR
+read -p "Elasticsearch heap size (never over 32!): " HEAP
+read -p "Cluster Name:?": CLUSTER
+version='Please choose elastic stack version: '
+options=("2.4" "5.6.7" "6.2.4")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "2.4")
+            version=2.4
+            kibana_version=4.2
+            break
+            ;;
+        "5.6.7")
+            version=5.6.7
+            echo "elasticstack 5.6.7"
+            break
+            ;;
+        "6.2.4")
+            version=6.2.4
+            echo "elasticstack 6.2.4"
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
+#pull necesary container images
+yum install -y docker
+docker pull docker.elastic.co/elasticsearch/elasticsearch:$version
+docker pull docker.elastic.co/kibana/kibana:$version
+#Configure rhel to run containers
+mkdir -p $DATA_DIR
+chmod -R 777 $DATA_DIR
+service docker stop
+sudo sysctl -w vm.max_map_count=262144
+service docker start
+echo "net.ipv4.conf.all.forwarding=1" >> /usr/lib/sysctl.d/00-system.conf
+echo "vm.max_map_count=1073741824" >> /usr/lib/sysctl.d/00-system.conf
+#deploy Elastic containers
+COUNTER=1
+while [  $COUNTER -le $NODES ]; do
+  read -p "interface-$COUNTER IP address?: " CONTAINER_IP
+  echo -n ""$CONTAINER_IP", "  >> iplist.txt
+  ./interface.sh $INTERFACE $CONTAINER_IP $COUNTER
+  ./docker.sh $CONTAINER_IP $COUNTER $DOMAIN_NAME $DATA_DIR $HEAP $CLUSTER $COUNTER $NODES $version
+  let COUNTER=COUNTER+1
+done
+~
--- docker.sh
+++ docker.sh
@@ -1,24 +1,24 @@
 # Docker.sh
 #
 # This script builds and clusters Elastic Stack (elasticsearch instances and kibana).
-# Usage:           1               2       3           4            5       6       7         8
-# ./docker.sh <IP of container> <Node#> <domain> <data directory> <HEAP> <cluster><counter><Nodes>
+# Usage:           1               2       3           4            5       6       7         8     9
+# ./docker.sh <IP of container> <Node#> <domain> <data directory> <HEAP> <cluster><counter><Nodes><version>
 HOSTS=$(cat iplist.txt)
 mkdir -p $4/elasticsearch_$2/data
 chmod -R 777 $4/elasticsearch_$2
 docker run -itd --name elasticsearch_$2 --restart=always \
 --privileged \
--v $4/data/elasticsearch_$2/data:/usr/share/elasticsearch/data \
+-v $4/elasticsearch_$2/data:/usr/share/elasticsearch/data/ \
 -e cluster.name=docker-$6 \
 -e node.name="elasticsearch_$2" \
--e "discovery.zen.ping.unicast.hosts=[$HOSTS]" \
+-e "discovery.zen.ping.unicast.hosts=$HOSTS" \
 -e ES_JAVA_OPTS="-Xms$5"g" -Xmx$5"g"" \
 -p $1:9200:9200 -p $1:9300:9300 \
-docker.elastic.co/elasticsearch/elasticsearch:5.6.5
+docker.elastic.co/elasticsearch/elasticsearch:$9
 
 if [ $7 == $8 ]
 then
   docker run -itd --name kibana --restart=always \
   --link elasticsearch_$2:elasticsearch -p $1:80:5601 \
-  docker.elastic.co/kibana/kibana:5.6.5
+  docker.elastic.co/kibana/kibana:$9
 fi
